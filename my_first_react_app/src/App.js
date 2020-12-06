dd5b062aefa25c319536d81241e194c38be3ee60
Syed Zeeshan Haider <kazmi9266@github.com>
2020-12-06 21:14:18 +0500

    initalcomments.html

---
+++ my_first_react_app/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Syed Zeeshan Haider
+        </p>
+        <a
+          className="App-link"
+          href="Syed Zeeshan Haider"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          My First React App
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
